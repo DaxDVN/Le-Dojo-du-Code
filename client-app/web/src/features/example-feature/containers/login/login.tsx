01ba64b0898684641181ba1b60447c25c46897f9
ndDuke <104844845+ndDuke@users.noreply.github.com>
2024-09-15 15:17:45 +0700

    Update login

--- client-app/web/src/features/example-feature/containers/login/login.tsx
+++ client-app/web/src/features/example-feature/containers/login/login.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
       <div className="flex items-center justify-center h-full">
         <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
-          <h1 className="text-center text-2xl font-semibold text-black text-4xl m-8">Login</h1>
+          <h1 className="text-center font-semibold text-black text-4xl m-8">Login</h1>
           <form className="space-y-4">
             <input
                 type="text"
@@ -19,25 +19,33 @@ export default function LoginPage() {
             />
 
             <div className="flex items-center">
-              <input type="checkbox" className="mr-2"/>
-              <span className="text-[#495057]">Remember me</span>
+              <input type="checkbox" className="mr-2 "/>
+              <span className="text-[#495057] text-sm">Remember me</span>
             </div>
-            <Button pill className="w-full bg-[#D96E48]">
+            <Button pill className="w-full border-none bg-[#D96E48] hover:bg-[#f29849] active:bg-[#f29849]">
               Log in
             </Button>
-            <p className="text-center text-black mt-2">Or Login Using</p>
-            <Button color="#D96E48" pill className="w-full p-0 flex items-center bg-[#E9ECEF] justify-center">
-              <img src={google_icon} alt="Google icon" className="mr-1 h-10 w-10" />
+            <div className="flex items-center">
+              <span className="flex-grow border-t border-gray-300"></span>
+              <p className="text-center text-black text-sm mx-4">Or Login Using</p>
+              <span className="flex-grow border-t border-gray-300"></span>
+            </div>
+
+            <Button color="#D96E48" pill
+                    className="w-full p-0 border-none flex items-center bg-[#E9ECEF] justify-center hover:bg-[#f1f3f5]">
+              <img src={google_icon} alt="Google icon" className="mr-1 h-10 w-10"/>
               <span className="text-[#000000] flex items-center">Continue with Google</span>
             </Button>
           </form>
           <p className="text-center mt-6">
-            <a href="#" className="text-blue-500 hover:underline text-[#495057]">
+            <a href="#" className=" text-[#D96E48] hover:text-[#f29849] hover:no-underline">
               Forgot password?
             </a>
           </p>
           <p className="text-center mt-6 mb-4">
-            <span className="text-black">Don't have an account?</span> <a href="#" className="text-blue-500 hover:underline">Sign up</a>
+            <span className="text-black">Don't have an account?</span> <a href="#"
+                                                                          className="text-[#D96E48] hover:text-[#f29849] hover:no-underline">Sign
+            up</a>
           </p>
         </div>
       </div>
