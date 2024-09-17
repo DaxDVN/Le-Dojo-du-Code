808ba37ec270e9a5116c88373a4b2fbfc27e0d73
ndDuke <104844845+ndDuke@users.noreply.github.com>
2024-09-17 14:01:12 +0700

    Add verify

--- client-app/web/src/features/example-feature/containers/sign-up/VerifyPage.tsx
+++ client-app/web/src/features/example-feature/containers/sign-up/VerifyPage.tsx
@@ -1,24 +1,26 @@
-import mailIcon from '../../../../assets/images/e-mail.svg'
-import {Button} from "flowbite-react";
-import AuthButtonComponent from "../../components/AuthButtonComponent.tsx";
+import mailIcon from '../../../../assets/images/mail.svg'
+import AuthButtonComponent from '../../components/AuthButtonComponent.tsx';
+import InputComponent from "../../components/InputComponent.tsx";
 
 export default function VerifyPage(props) {
-
-    const email = 'ducnguyen7925@gmail.com'
+    const email = 'ducnguyen7925@gmail.com';
 
     return (
-        <div className="flex items-center justify-center h-full">
-            <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
-                <h1 className="text-center font-semibold text-black text-4xl m-8">Sign Up</h1>
-                <span className='text-black text-center'> Please verify your email </span>
-                    <img src={mailIcon} alt="Mail"/>
-                <span className={'text-black p-2'}>
-                    We've sent an email to {email}  with instructions.
+        <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-full max-w-sm p-12 space-y-4 bg-white rounded-lg shadow-md flex flex-col items-center">
+                <h1 className="text-center font-semibold text-black text-4xl mb-10">Sign Up</h1>
+                <p className='text-black text-center'>Please verify your email</p>
+                {/*<img src={mailIcon} alt="Mail" className="mb-4"/>*/}
+                <div className="p-4 bg-[#b2f2bb] w-full mb-4 text-center">
+                    <p className='text-black text-sm'>
+                        We've sent an email to {email} with instructions.
+                    </p>
+                </div>
+                <InputComponent text='Enter code'/>
+                <span className='text-black'>
+                    Didn't receive an email? <a href='#' className='text-[#D96E48]'>Resend email</a>
                 </span>
-                <a href='https://mail.google.com/'>
-                    <AuthButtonComponent>Verify Email</AuthButtonComponent>
-                </a>
-
+                <a href={'#'} className='text-[#D96E48]'>Return to Login</a>
             </div>
         </div>
     );
