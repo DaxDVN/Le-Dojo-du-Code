3506bb1cda326d3be0690c0c3eb10a888cb3c235
daxnguyen-admin <dax.datnguyen@gmail.com>
2024-09-24 21:57:27 +0700

    Initial  admin feature

---
+++ client-app/web/src/features/admin/components/ExampleFeatureComponent.tsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+function ExampleFeatureComponent() {
+    return (
+        <div></div>
+    );
+}
+
+export default ExampleFeatureComponent;
---
+++ client-app/web/src/features/admin/containers/example-feature-page/ExampleFeaturePage.tsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+function ExampleFeaturePage() {
+    return (
+        <div></div>
+    );
+}
+
+export default ExampleFeaturePage;
